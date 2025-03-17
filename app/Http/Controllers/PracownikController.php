80f2ce6c429fefec2201c160792ae1f57d645e94
jbobrowski <jbobrowski@loreto.ac.uk>
2025-03-17 00:10:30 +0000

    created and ran migrations; updated and created firma and pracownik tables with necessary fields

---
+++ app/Http/Controllers/PracownikController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Pracownik;
+use Illuminate\Http\Request;
+
+class PracownikController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Pracownik  $pracownik
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Pracownik $pracownik)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Pracownik  $pracownik
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Pracownik $pracownik)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Pracownik  $pracownik
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Pracownik $pracownik)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Pracownik  $pracownik
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Pracownik $pracownik)
+    {
+        //
+    }
+}
---
+++ database/migrations/2025_03_17_000233_create_firmas_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('firmas', function (Blueprint $table) {
+            $table->id();
+            $table->string('nazwa');
+            $table->string('NIP');
+            $table->string('adres');
+            $table->string('miasto');
+            $table->string('kod_pocztowy');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('firmas');
+    }
+};
